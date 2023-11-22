06d7295ec7dc5c773d25add3d3f2d25b1005c636
Itai Levi <italev46@gmail.com>
2023-11-22 11:45:34 -0500

    feat(org): add get/set for active org ID

--- src/org.ts
+++ src/org.ts
@@ -1,3 +1,6 @@
+import { ACTIVE_ORG_ID_COOKIE_NAME } from "@propelauth/backend-js-utilities"
+import Cookies from "js-cookie"
+
 export type OrgMemberInfo = {
     orgId: string
     orgName: string
@@ -8,4 +11,12 @@ export type OrgMemberInfo = {
 }
 export type OrgIdToOrgMemberInfo = {
     [orgId: string]: OrgMemberInfo
+}
+
+export const setActiveOrgId = (orgId: string) => {
+    Cookies.set(ACTIVE_ORG_ID_COOKIE_NAME, orgId)
+}
+
+export const getActiveOrgId = (): string | undefined => {
+    return Cookies.get(ACTIVE_ORG_ID_COOKIE_NAME)
 }
