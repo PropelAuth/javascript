b5357bc93bda991d2204cddca5617ab3058b8aa4
Itai Levi <italev46@gmail.com>
2023-12-18 16:09:01 -0500

    feat(client): add user signup query parameters option to login url as well

--- src/client.ts
+++ src/client.ts
@@ -12,7 +12,8 @@ export interface RedirectToSignupOptions {
 }
 
 export interface RedirectToLoginOptions {
-    postLoginRedirectUrl: string
+    postLoginRedirectUrl?: string
+    userSignupQueryParameters?: Record<string, string>
 }
 
 export interface IAuthClient {
@@ -286,12 +287,24 @@ export function createClient(authOptions: IAuthOptions): IAuthClient {
     }
 
     const getLoginPageUrl = (options?: RedirectToLoginOptions) => {
-        let qs = ""
-        if (options && options.postLoginRedirectUrl) {
+        let qs = new URLSearchParams()
+        let url = `${clientState.authUrl}/login`
+        if (options) {
             const encode = window ? window.btoa : btoa
-            qs = new URLSearchParams({ rt: encode(options.postLoginRedirectUrl) }).toString()
+            const { postLoginRedirectUrl, userSignupQueryParameters } = options
+            if (postLoginRedirectUrl) {
+                qs.set("rt", encode(postLoginRedirectUrl))
+            }
+            if (userSignupQueryParameters) {
+                Object.entries(userSignupQueryParameters).forEach(([key, value]) => {
+                    qs.set(key, value)
+                })
+            }
         }
-        return `${clientState.authUrl}/login?${qs}`
+        if (qs.toString()) {
+            url += `?${qs.toString()}`
+        }
+        return url
     }
 
     const getAccountPageUrl = () => {
